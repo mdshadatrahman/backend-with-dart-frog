65860e75652dc652965db377f52f0c979101d697
shadat <shadat.rahman.464@gmail.com>
2023-04-19 11:44:59 +0600

    db package created

---
+++ packages/db/lib/db.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library db;
+
+export 'src/db_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
